015169c5dbbc010360dcb2459d1ae6c6704881f2
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-07-09 23:38:36 +0500

    codingwars

---
+++ app.js
@@ -0,0 +1,91 @@
+
+// {
+//     function greet() {
+//         return "hello world!";
+//     }   
+//     console.log(greet());
+// }
+
+// {
+//     function countSheep(sheep){
+//         return sheep.filter(qoy => qoy).length
+//     }
+    
+//     console.log(countSheep([undefined]));
+//     console.log(countSheep([undefined, null, false, true]));
+// }
+
+// {
+//         const removeChar = (str) => str.length <= 2 ? str : str.slice(1, str.length-1)
+        
+//         console.log( removeChar("salom"));
+// }
+
+// {
+//     function findBigInt(args){
+//         args.sort((a,b) => a-b)
+//         return `${args[0]} ${args[args.length-1]}`
+//     }
+
+//     console.log(findBigInt([20,15,1,8]));
+// }
+
+// {
+//     const assert = require('assert');
+
+// function reverseNumber(num) {
+//     return parseInt(num.toString().split('').reverse().join(''), 10);
+// }
+
+// console.log(reverseNumber(2458));
+// // assert.strictEqual(reverseNumber(215638), 865321);
+
+
+// }
+
+// {
+//     function descendingOrder(n){
+//         return n.toString().split('').reverse()
+//     }
+
+//     console.log(descendingOrder(14584));
+// }
+
+// {
+//     function removeChar(str){
+//         return str.slice(1,-1)
+//     };
+
+//     console.log( removeChar("country"));
+// }
+
+// {
+//     function solution(str){
+//         return str.split('').reverse().join('');
+//       }
+
+// }
+
+// {
+//     function getDivisorsCnt(n){
+//         return n%2 =0
+//     }
+
+//     // assert.strictEqual(getDivisorsCnt(11), 2);
+//     console.log( getDivisorsCnt(11));
+// }
+
+// {
+//     function getDivisorsCnt(num) {
+//         let count = 0;
+//         for (let i = 1; i <= num; i++) {
+//             if (num % i === 0) {
+//                 count++;
+//             }
+//         }
+//         return num ;
+//     }
+
+//         console.log( getDivisorsCnt(11));
+
+// }
